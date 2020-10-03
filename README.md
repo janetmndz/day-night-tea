7aeea842ac54de7de7a37e3c84ffbb65cfdb19ee
Janet <janetmendez.03@gmail.com>
2020-10-03 13:36:32 -0400

    doing some housekeeping

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Day and Night Teacup
+<h1 align="center">Day and Night Teacup ☀ 🌙 ☕ </h1>
+
+<div align="center">
+  <sub>Built with ❤️ and ✨ by
+  <a href="https://github.com/janetmndz">Janet Mendez</a>
+  </sub>
+</div>
+
+<br />
+
+
+This CSS art animation is made with:
+- Pug - HTML templating engine
+- SCSS - CSS pre-processor
+- Gulp - task runner
--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,38 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const pug = require('gulp-pug');
+const { series } = require('gulp');
 const browserSync = require('browser-sync').create();
 
 const style = () => {
-  return gulp.src('./*.scss')
+  return gulp.src('./src/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest('./src/'))
     .pipe(browserSync.stream());
 }
 
 const view = () => {
-  return gulp.src('./*.pug')
+  return gulp.src('./src/**/*.pug')
   .pipe(pug({
     pretty: true
   }))
-  .pipe(gulp.dest('./'))
+  .pipe(gulp.dest('./src'))
   .pipe(browserSync.stream())
 }
 
 const watch = () => {
   browserSync.init({
     server: {
-      baseDir: './'
+      baseDir: './src'
     }
   });
-  gulp.watch('./*.scss', style);
-  gulp.watch('./*.pug', view);
-  gulp.watch('./*.html')
+  gulp.watch('./src/**/*.scss', style);
+  gulp.watch('./src/**/*.pug', view);
+  gulp.watch('./src/**/*.html')
     .on('change', browserSync.reload);
 }
 
 exports.style = style;
 exports.view = view;
 exports.watch = watch;
+exports.build = series(style, view, watch);
